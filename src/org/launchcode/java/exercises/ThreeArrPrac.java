ba4b4fa8addf22be7439dcdf64ca527ae1be2362
Sarah <abantaos@hotmail.com>
2020-08-30 12:42:52 -0500

    two

--- src/org/launchcode/java/exercises/ThreeArrPrac.java
+++ src/org/launchcode/java/exercises/ThreeArrPrac.java
@@ -8,24 +8,24 @@ public class ThreeArrPrac {
 
 //      Print numbers in array
 
-          for (int i : nums) {
-              System.out.println(i);
-          }
+        for (int i : nums) {
+            System.out.println(i);
+        }
 
 //      Print odd numbers in array
 
-            for (int i=0; i < nums.length; i++) {
-                if (nums[i] % 2 != 0) {
-                    System.out.println(nums[i]);
-                }
+        for (int i=0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                System.out.println(nums[i]);
             }
+        }
 
 //      Print separated by sentences
 
-            String str = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+        String str = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
 
-            String[] arr = str.split("\\.");
+        String[] arr = str.split("\\.");
 
-            System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
     }
 }
